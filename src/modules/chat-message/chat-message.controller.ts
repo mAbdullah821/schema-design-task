2c9240091dc54ae6ee6ff8a269147d8d6ad0b4bb
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-03 20:21:32 +0300

    Add Get endpoint to Chat-Message

--- src/modules/chat-message/chat-message.controller.ts
+++ src/modules/chat-message/chat-message.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ChatMessageService } from './chat-message.service';
 
 @Controller('chat-message')
 export class ChatMessageController {
   constructor(private readonly chatMessageService: ChatMessageService) {}
+
+  @Get()
+  sayHello() {
+    return this.chatMessageService.getHelloMessage();
+  }
 }
--- src/modules/chat-message/chat-message.service.ts
+++ src/modules/chat-message/chat-message.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ChatMessageService {}
+export class ChatMessageService {
+  getHelloMessage() {
+    return { message: 'Hello, From Chat-Message-Service' };
+  }
+}
