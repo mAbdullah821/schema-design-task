39064de596c69e8b8d56c4f12bf7256bde3aa503
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-03 20:24:52 +0300

    Add Get endpoint to household-orders

--- src/modules/orders/household-orders/household-orders.controller.ts
+++ src/modules/orders/household-orders/household-orders.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HouseholdOrdersService } from './household-orders.service';
 
 @Controller('household-orders')
 export class HouseholdOrdersController {
-  constructor(private readonly householdOrdersService: HouseholdOrdersService) {}
+  constructor(
+    private readonly householdOrdersService: HouseholdOrdersService,
+  ) {}
+
+  @Get()
+  sayHello() {
+    return this.householdOrdersService.getHelloMessage();
+  }
 }
--- src/modules/orders/household-orders/household-orders.service.ts
+++ src/modules/orders/household-orders/household-orders.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HouseholdOrdersService {}
+export class HouseholdOrdersService {
+  getHelloMessage() {
+    return { message: 'Hello, From Household-Orders-Service' };
+  }
+}
