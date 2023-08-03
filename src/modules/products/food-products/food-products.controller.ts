8e8acd332dcdd9cc7393278b3c0bfdb19bd509db
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-03 20:25:53 +0300

    Add Get endpoint to food-products

--- src/modules/products/food-products/food-products.controller.ts
+++ src/modules/products/food-products/food-products.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { FoodProductsService } from './food-products.service';
 
 @Controller('food-products')
 export class FoodProductsController {
   constructor(private readonly foodProductsService: FoodProductsService) {}
+
+  @Get()
+  sayHello() {
+    return this.foodProductsService.getHelloMessage();
+  }
 }
--- src/modules/products/food-products/food-products.service.ts
+++ src/modules/products/food-products/food-products.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FoodProductsService {}
+export class FoodProductsService {
+  getHelloMessage() {
+    return { message: 'Hello, From Food-Products-Service' };
+  }
+}
