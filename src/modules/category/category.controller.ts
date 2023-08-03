622a8a1e1d63660b5c2fda8ca4213b0f3fe4d97f
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-03 20:20:45 +0300

    Add Get endpoint to Category

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CategoryService } from './category.service';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  sayHello() {
+    return this.categoryService.getHelloMessage();
+  }
 }
