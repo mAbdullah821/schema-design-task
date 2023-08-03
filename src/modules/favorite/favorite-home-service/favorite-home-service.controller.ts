2318479a61cb182f697b410e07e03ccb2ba831ac
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-03 20:22:48 +0300

    Add Get endpoint to favorite-home-service

--- src/modules/favorite/favorite-home-service/favorite-home-service.controller.ts
+++ src/modules/favorite/favorite-home-service/favorite-home-service.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { FavoriteHomeServiceService } from './favorite-home-service.service';
 
 @Controller('favorite-home-service')
 export class FavoriteHomeServiceController {
-  constructor(private readonly favoriteHomeServiceService: FavoriteHomeServiceService) {}
+  constructor(
+    private readonly favoriteHomeServiceService: FavoriteHomeServiceService,
+  ) {}
+
+  @Get()
+  sayHello() {
+    return this.favoriteHomeServiceService.getHelloMessage();
+  }
 }
--- src/modules/favorite/favorite-home-service/favorite-home-service.service.ts
+++ src/modules/favorite/favorite-home-service/favorite-home-service.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FavoriteHomeServiceService {}
+export class FavoriteHomeServiceService {
+  getHelloMessage() {
+    return { message: 'Hello, From Favorite-Home-Service-Service' };
+  }
+}
