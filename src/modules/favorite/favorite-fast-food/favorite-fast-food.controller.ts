42943ec986b701ff5fbe34c22d5b06b524c981e7
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-03 20:22:17 +0300

    Add Get endpoint to favorite-fast-food

--- src/modules/favorite/favorite-fast-food/favorite-fast-food.controller.ts
+++ src/modules/favorite/favorite-fast-food/favorite-fast-food.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { FavoriteFastFoodService } from './favorite-fast-food.service';
 
 @Controller('favorite-fast-food')
 export class FavoriteFastFoodController {
-  constructor(private readonly favoriteFastFoodService: FavoriteFastFoodService) {}
+  constructor(
+    private readonly favoriteFastFoodService: FavoriteFastFoodService,
+  ) {}
+
+  @Get()
+  sayHello() {
+    return this.favoriteFastFoodService.getHelloMessage();
+  }
 }
--- src/modules/favorite/favorite-fast-food/favorite-fast-food.service.ts
+++ src/modules/favorite/favorite-fast-food/favorite-fast-food.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FavoriteFastFoodService {}
+export class FavoriteFastFoodService {
+  getHelloMessage() {
+    return { message: 'Hello, From Favorite-Fast-Food-Service' };
+  }
+}
