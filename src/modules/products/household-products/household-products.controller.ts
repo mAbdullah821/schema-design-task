d825be03b01f8bd0134056e6a8164aa71bc99612
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-03 20:26:21 +0300

    Add Get endpoint to household-products

--- src/modules/products/household-products/household-products.controller.ts
+++ src/modules/products/household-products/household-products.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HouseholdProductsService } from './household-products.service';
 
 @Controller('household-products')
 export class HouseholdProductsController {
-  constructor(private readonly householdProductsService: HouseholdProductsService) {}
+  constructor(
+    private readonly householdProductsService: HouseholdProductsService,
+  ) {}
+
+  @Get()
+  sayHello() {
+    return this.householdProductsService.getHelloMessage();
+  }
 }
--- src/modules/products/household-products/household-products.service.ts
+++ src/modules/products/household-products/household-products.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HouseholdProductsService {}
+export class HouseholdProductsService {
+  getHelloMessage() {
+    return { message: 'Hello, From Household-Products-Service' };
+  }
+}
