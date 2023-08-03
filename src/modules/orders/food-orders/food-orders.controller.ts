28cf8cd5861f5c787f91c6eb0e132431a678871b
mAbdullah821 <mahmoud.abdullah821@gmail.com>
2023-08-03 20:24:09 +0300

    Add Get endpoint to food-orders

--- src/modules/orders/food-orders/food-orders.controller.ts
+++ src/modules/orders/food-orders/food-orders.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { FoodOrdersService } from './food-orders.service';
 
 @Controller('food-orders')
 export class FoodOrdersController {
   constructor(private readonly foodOrdersService: FoodOrdersService) {}
+
+  @Get()
+  sayHello() {
+    return this.foodOrdersService.getHelloMessage();
+  }
 }
